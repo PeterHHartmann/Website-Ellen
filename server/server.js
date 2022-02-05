559cca1c9e20d490cdaefa1bc95fca0936332424
Peter <peter.helle.hartmann@gmail.com>
2022-02-05 20:21:12 +0100

    fixed a typo

--- server/server.js
+++ server/server.js
@@ -15,9 +15,6 @@ if(process.env.NODE_ENV === 'production') {
 
 app.use(express.static(buildPath));
 
-
-app.use(redirectHTTTP);
-
 app.get('*', (req, res) => {
     res.sendFile(path.join(buildPath, 'index.html'));
 });
